b2ef3d34129307ce54e68bbe259d6b48c70361bd
Sebastian-Rodriguez <sebasrz.rcia@gmail.com>
2019-06-07 15:51:03 -0300

    Agregando nuevos componentes, Deportes, Espectaculos, Internacionales, Politica y Teconologia. Todos tienen la misma estructura que Home, solo cambia la ruta del fetch

---
+++ src/components/Deportes/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Card from '../Card';
+import { CardContainer, Title }from './styled';
+
+
+class Deportes extends Component{
+    constructor(){
+        super()
+        this.state = {
+            isLoading: false,
+            data: []
+        }
+    }
+
+    async componentDidMount(){
+        const url = "https://api.canillitapp.com/news/category/5";
+        this.setState({ isLoading: true });
+
+        try{
+            const response = await fetch(url);
+            const data = await response.json();
+    
+            this.setState({ data: data.slice(0,10) });
+            console.log(this.state.data);
+            
+        }catch(error){
+            console.log(error);
+        }finally{
+            this.setState({ isLoading: false })
+        }
+
+
+    }
+
+    render(){
+        const { isLoading, data } = this.state;
+        
+        console.log(data);
+
+        return(
+            <div>
+                <Title> Deportes </Title>
+                <CardContainer>
+                    { 
+                        isLoading ? <p>cargando</p> : data.map( data => (
+                            <Card title={ data.title} image={ data.img_url } source={ data.source_name } url={ data.url }></Card>
+                        ))
+                    
+                    }            
+                </CardContainer>
+            </div>
+        )
+    }
+
+}
+
+export default Deportes;
