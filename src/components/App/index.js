fd8f6eb42694cdc3d89b8db17567558b87bc4a9b
Sebastian-Rodriguez <sebasrz.rcia@gmail.com>
2019-06-07 15:39:02 -0300

    Agregando los componentes necesarios para la navegacion en el componente App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css'
 
 //components
 import Header from '../Header';
 import SideBar from '../SideBar';
-import ContentWrapper from '../ContentWrapper';
+
+//pages
 import Home from '../Home';
+import Deportes from '../Deportes';
+import Politica from '../Politica';
+import Espectaculos from '../Espectaculos';
+import Tecnologia from '../Tecnologia';
+import Internacionales from '../Internacionales';
 
 function App(){
   return (
-        <div className="App">
-          <Header />
-          <div className="container">
-            <SideBar />
-            
-            <Home></Home>
+        <Router>
+          <div className="App">
+            <Header />
+            <div className="container">
+              <SideBar />
+              <Route exact path="/" component={ Home }/>
+              <Route path="/deportes" component={ Deportes }/>
+              <Route path="/espectaculos" component={ Espectaculos }/>
+              <Route path="/tecnologia" component={ Tecnologia }/>
+              <Route path="/politica" component={ Politica }/>
+              <Route path="/internacionales" component={ Internacionales }/>
+            </div>
           </div>
-        </div>
+        </Router>
       );
 }
 
