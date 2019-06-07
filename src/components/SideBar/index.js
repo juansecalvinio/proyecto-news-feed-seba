c0b3da34f4d47eb638ac23ec2d7ad858f534f166
Sebastian-Rodriguez <sebasrz.rcia@gmail.com>
2019-06-07 15:37:04 -0300

    Agregando Links de cada seccion al componente SideBar

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import SideBarWrapper from './styled';
 
 const SideBar = () => {
     return(
-        <SideBarWrapper>
-            <ul>
-                <li>Option</li>
-                <li>Option</li>
-                <li>Option</li>
-                <li>Option</li>
-                <li>Option</li>
-            </ul>
-        </SideBarWrapper>
+            <SideBarWrapper>
+                <Link to="/">Home</Link>
+                <Link to="/politica">Politica</Link>
+                <Link to="/internacionales">Internacionales</Link>
+                <Link to="/tecnologia">Tecnologia</Link>
+                <Link to="/espectaculos">Espectaculos</Link>
+                <Link to="/deportes">Deportes</Link>
+            </SideBarWrapper>
     )
 }
 
