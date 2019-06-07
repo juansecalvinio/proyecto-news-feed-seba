68b98dd4aee8c825df6ce23ccdbba07010d3a100
Sebastian-Rodriguez <sebasrz.rcia@gmail.com>
2019-06-07 15:33:27 -0300

    Cambiando estilos del componente Sidebar

--- src/components/SideBar/styled.js
+++ src/components/SideBar/styled.js
@@ -1,10 +1,12 @@
-import Style from 'styled-components';
+import styled from 'styled-components';
 
-const SideBarWrapper = Style.div`
+const SideBarWrapper = styled.div`
     height:542px;
     width: 20%;
-    background-color: #383838;
+    background-color: #f6f6f6;
     padding: 20px;
+    display: flex;
+    flex-direction: column;
     
 `
 
